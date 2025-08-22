a596f35c3e0a7ae6f2c08364d3c325f3baa749d2
Ayan Shaikh <richieshaikh5691@gmail.com>
2025-08-22 16:33:12 +0530

    Added calculator.java

---
+++ calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class calculator{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+int a,b;
+System.out.print("Enter Two Numbers :");
+a = sc.nextInt();
+b = sc.nextInt();
+System.out.printIn("Sum =" + (a + b));
+sc.close();
+}
+}
